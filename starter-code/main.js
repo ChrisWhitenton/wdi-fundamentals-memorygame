73c62cd1128fdbfb6920ba8695b95eb152d7a3e6
Deathly Hallows <chriswhitenton@Deathly-Hallows.local>
2017-03-01 11:42:24 -0500

    added alert you have a match

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,12 @@ var cardOne='queen';
 var cardTwo='queen';
 var cardThree='king';
 var cardFour='king';
+
+var cardTwo='queen'; cardFour='king'; cardOne='queen'; cardThree='king';
+
+	if (cardTwo === cardFour && cardOne === cardThree){ 
+	result = alert ("Sorry, try again.");
+	}
+	else if  (cardOne === cardTwo && cardThree === cardFour){
+	result = alert ("You found a match!");
+	}
