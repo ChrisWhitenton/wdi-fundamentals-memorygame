209318674cf52673510c9a1aa618e408f7a262f7
Deathly Hallows <chriswhitenton@Deathly-Hallows.local>
2017-03-03 23:09:18 -0500

    final attempt

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,56 @@
-var cardOne='queen';
-var cardTwo='queen';
-var cardThree='king';
-var cardFour='king';
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-var cardTwo='queen'; cardFour='king'; cardOne='queen'; cardThree='king';
 
-	if (cardTwo === cardFour && cardOne === cardThree){ 
-	result = alert ("Sorry, try again.");
+
+var currentScore = document.getElementById('score');
+
+var createBoard = function () { 
+	for (var i = 0; i < cards.length; i++) {
+	var newCard = document.createElement('div');
+		newCard.setAttribute('data-card', cards[i]);
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+		newCard.addEventListener('click', isTwoCards);
 	}
-	else if  (cardOne === cardTwo && cardThree === cardFour){
-	result = alert ("You found a match!");
+}
+createBoard();
+
+
+
+var gameBoard = document.getElementById('game-board');
+var allCards = document.getElementsByClassName('card');
+var currentScore = document.getElementById('score');
+
+var createCards = function() {
+	for(var i=0; i<4; i++);
+	var cardDiv = document.createElement("div");
+	cardDiv.className = 'card';
+	gameBoard.appendChild(cardDiv);	
+}
+createCards();
+
+
+var isMatch = function() {
+   if (cardsInPlay[0] === cardsInPlay[1]) {
+     score += 1;
+     tallyElement.innerHTML = score;
+     setTimeout(function(){
+       alert ("You found a match!")
+   } else {
+     setTimeout(function) 
+       alert("Sorry, no match.");
+     }
+   }
+
+var isTwoCards = function () {
+   	cardsInPlay.push(this.getAttribute('data-card'));
+   	if (this.getAttribute('data-card') === 'king') {
+			this.innerHTML = '<img src="king of hearts.jpeg" alt="King of Hearts" />';
+	   } else {
+			this.innerHTML = '<img src="queen of hearts.jpg" alt="Queen of Clubs" />';
+		}
+		if (cardsInPlay.length === 2) {
+			isMatch(cardsInPlay);
+			cardsInPlay = [];
 	}
